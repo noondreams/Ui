01af7b248c91776237837d134b8c4ee23f1427e7
Mohammad Rahmani <web@noondreams.com>
2014-02-18 06:16:02 +0330

    Sample Added

--- MLM/Hor/MLMHor.js
+++ MLM/Hor/MLMHor.js
@@ -1,21 +1,23 @@
 function mainmenu() {
-    $("#publicMenu ul ").css({display: "none"}); // Opera Fix
-    $("#publicMenu li").hover(function() {
+    $(".MLMHor ul ").css({display: "none"}); // Opera Fix
+    $(".MLMHor li").hover(function() {
         $(this).find('ul:first').css({visibility: "visible", display: "none"}).show(200);
     }, function() {
         $(this).find('ul:first').css({visibility: "hidden"});
     });
 
-    $("#publicMenu>li").each(function() {
+    $(".MLMHor>li").each(function() {
         $this = $(this);//the first row <li>s
         if ($this.children('ul').length > 0)
             $this.children("a").addClass("bgDown");
     });
 
-    $("#publicMenu>li ul>li").each(function() {
+    $(".MLMHor>li ul>li").each(function() {
         $this = $(this);//the first row <li>s
         if ($this.children('ul').length > 0)
             $this.children("a").addClass("bgSide");
     });
 }
-mainmenu();
+$(document).ready(function() {
+    mainmenu();
+});
